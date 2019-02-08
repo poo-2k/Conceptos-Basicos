f347a0f99973de62e6792e13cf6ca57b2b6fad9a
SamantaGarcia <samantarg2000@hotmail.com>
2019-02-07 19:16:01 -0600

    metodos

--- auto.js
+++ auto.js
@@ -1,5 +1,5 @@
 export default class Car{
-    constructor(marca, modelo, color, kilometraje){
+    constructor(marca, modelo, color = "white", kilometraje = 0){
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
@@ -10,5 +10,25 @@ export default class Car{
     showStatus(){
        // forma antigua: console.log("The car" + this.marca + "" + this.color);
         console.log(`The ${this.color} car ${this.marca} of the year ${this.modelo} has a mileage of ${this.kilometraje} kms and is ${this.status}`);
+    }
+
+    turnOn(){
+        this.status = "On";
+        this.showStatus();
+    }
+
+    turnOff(){
+        this.status = "Off";
+        this.showStatus();
+    }
+
+    go(speed, time){
+        if(this.status === "Off") {
+            console.log("The car can not move along because was off");
+        } else{
+            let long = speed * time;
+            this.kilometraje = this.kilometraje + long;
+            this.showStatus();
+                }
     }
 }
--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 import Car from "./auto.js";
 
 let car1 = new Car("Ford", 2000, "red", 100);
-car1.showStatus();
+let car2 = new Car("Nissan", 2010, "blue");
+let car3 = new Car("Toyota", 2018);
+
+car1.turnOn();
+car1.go(50,1.5);
+car1.turnOff();
+car1.go(50,1.5);
+
+console.log(`Mileage ${car1.kilometraje}`);
+car1.kilometraje = 5;
+console.log(`Mileage ${car1.kilometraje}`);
+
+
+// car2.turnOn();
+// car2.turnOff();
+
+// car3.turnOn();
+// car3.turnOff();
