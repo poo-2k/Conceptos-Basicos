679c3015c160d1fd1aa3511c780f71e8fffe2ec3
SamantaGarcia <samantarg2000@hotmail.com>
2019-02-07 18:45:01 -0600

    the red car

--- auto.js
+++ auto.js
@@ -1,9 +1,14 @@
-class Auto{
+export default class Car{
     constructor(marca, modelo, color, kilometraje){
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
         this.kilometraje = kilometraje;
         this.status = "Off";
+    }
+
+    showStatus(){
+       // forma antigua: console.log("The car" + this.marca + "" + this.color);
+        console.log(`The ${this.color} car ${this.marca} of the year ${this.modelo} has a mileage of ${this.kilometraje} kms and is ${this.status}`);
     }
 }
--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("Hola mundo")
+import Car from "./auto.js";
+
+let car1 = new Car("Ford", 2000, "red", 100);
+car1.showStatus();
